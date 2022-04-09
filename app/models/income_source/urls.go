f4450fefa6595dcac90e683d7af91a7cf1ab649b
Andres Camilo Ramos Romero <andrescramosr@gmail.com>
2022-04-08 23:37:30 -0500

    Create Source URI and imporvements to GET URI`s

--- app/models/income_source/urls.go
+++ app/models/income_source/urls.go
@@ -5,4 +5,5 @@ import "github.com/AndresCRamos/Simple-Personal-Finances/utils"
 var SourcesRoutes = []utils.Route{
 	{Url: "/api/v1/source", Handler: GetIncomeSourcesByUserID, Method: "GET"},
 	{Url: "/api/v1/source/{id}", Handler: GetIncomeSourcesByID, Method: "GET"},
+	{Url: "/api/v1/source", Handler: CreateIncomeSource, Method: "POST"},
 }
--- app/models/income_source/view.go
+++ app/models/income_source/view.go
@@ -2,40 +2,46 @@ package incomesource
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/AndresCRamos/Simple-Personal-Finances/utils"
 	"github.com/gorilla/mux"
 )
 
-func checkIfExists(id string) (IncomeSource, bool) {
-	var source IncomeSource
-	utils.Instance.First(&source, id)
-	fmt.Println(source)
-	exists := false
-	if source.ID != 0 {
-		exists = true
-	}
-	return source, exists
-}
-
 func GetIncomeSourcesByUserID(w http.ResponseWriter, r *http.Request) {
 	var incomeSources []IncomeSource
-	utils.Instance.Find(&incomeSources)
+	if err := utils.Instance.Find(&incomeSources).Error; err != nil {
+		utils.DisplaySearchError(w, r, "Sources", err.Error())
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(incomeSources)
 }
 
 func GetIncomeSourcesByID(w http.ResponseWriter, r *http.Request) {
+	var source IncomeSource
 	sourceId := mux.Vars(r)["id"]
-	source, exists := checkIfExists(sourceId)
-	if exists {
+	if err := utils.Instance.First(&source, sourceId).Error; err != nil {
+		utils.DisplaySearchError(w, r, "Sources", err.Error())
+	} else {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(source)
+	}
+}
+
+func CreateIncomeSource(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var incomeSource IncomeSource
+	json.NewDecoder(r.Body).Decode(&incomeSource)
+	errorList, valid := utils.Validate(incomeSource)
+	if !valid {
+		utils.DisplayFieldErrors(w, r, "Source", errorList)
+	} else if err := utils.Instance.Create(&incomeSource).Error; err != nil {
+		utils.DisplaySearchError(w, r, "Sources", err.Error())
 	} else {
-		utils.DisplayError(w, r, "Sources", fmt.Sprintf("Source with id: %s does not exist", sourceId))
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(incomeSource)
 	}
 }
